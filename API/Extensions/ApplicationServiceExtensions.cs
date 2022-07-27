586787c1c81d5d6575313dcf598118b7f22ff35b
Abhijit Dhobale <abhijit.dhobale@mobifilia.com>
2022-07-27 16:58:07 +0530

    section four completed

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+  public static class ApplicationServiceExtensions
+  {
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+      /** Add Token service */
+      services.AddScoped<ITokenService, TokenService>();
+
+      // Add services to the container.
+      services.AddDbContext<API.Data.DataContext>(options =>
+      {
+        options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+      });
+
+      return services;
+    }
+  }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,18 @@
+using System.Text;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<API.Data.DataContext>(options =>
-{
-  options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddApplicationServices(builder.Configuration);
+
 builder.Services.AddControllers();
+
+builder.Services.AddIdentityServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,6 +27,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
